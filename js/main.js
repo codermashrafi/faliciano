1a57af7c1fa60a8b2ec07c83829b5a94a580b97b
Mashrafi Mortuza <141340072+codermashrafi@users.noreply.github.com>
2023-09-20 22:47:21 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: true,
+});
+AOS.init();
